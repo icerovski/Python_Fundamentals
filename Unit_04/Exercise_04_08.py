0f90f9c57a3955cbf6d02cfb0f0f69d47c185fc6
iCerovski <ivan.cerovski@gmail.com>
2019-01-15 12:45:26 +0200

    Unit 04 - progressed to exercise 8

--- Unit_04/Exercise_04_08.py
+++ Unit_04/Exercise_04_08.py
@@ -1,1 +1,14 @@
 # 08. Append Lists
+
+str_list = list(input().split('|'))
+
+
+def split_str(a):
+    result = " ".join(a.split())
+    return result
+
+
+new_str_list = [split_str(el) for el in reversed(str_list)]
+
+
+print(' '.join(new_str_list))
