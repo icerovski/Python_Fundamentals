dc6f80bb9a1c55a78e1bfc0971a1325a6d58bb50
iCerovski <ivan.cerovski@gmail.com>
2019-01-15 14:48:17 +0200

    Unit 04 - finished Lists

--- Unit_04/Exercise_04_08.py
+++ Unit_04/Exercise_04_08.py
@@ -2,13 +2,20 @@
 
 str_list = list(input().split('|'))
 
+# Empty strings are considered false in a Boolean context
+def is_not_blank(my_string):
+    if my_string and my_string.strip():
+        # my_string is not None AND my_string is not empty or blank
+        return True
+    # my_string is None OR my_string is empty or blank
+    return False
+
 
 def split_str(a):
     result = " ".join(a.split())
     return result
 
 
-new_str_list = [split_str(el) for el in reversed(str_list)]
-
+new_str_list = [split_str(el) for el in reversed(str_list) if is_not_blank(el)]
 
 print(' '.join(new_str_list))
