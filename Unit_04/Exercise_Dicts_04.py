c1b535c68741fe45d686c78231b106d0c8a0da76
iCerovski <ivan.cerovski@gmail.com>
2019-01-16 20:56:03 +0200

    Unit 04 - started Dictionaries, just finishing Exercise 06

--- Unit_04/Exercise_Dicts_04.py
+++ Unit_04/Exercise_Dicts_04.py
@@ -1,12 +1,6 @@
 # 04. Dict-Ref
 entries = {}
 
-
-def in_dict(ch):
-    if ch in entries:
-        return True
-
-
 while True:
     line = input()
     if line == "end":
@@ -21,7 +15,7 @@ while True:
             entries[name] = second_name
         except ValueError:
             second_name = new_line[1]
-            if in_dict(second_name):
+            if second_name in entries:
                 entries[name] = entries[second_name]
 
 for key, value in entries.items():
--- Unit_04/Exercise_Dicts_05.py
+++ Unit_04/Exercise_Dicts_05.py
@@ -1,2 +1,21 @@
 # 05. Mixed Phones
 
+phone_book = {}
+
+while True:
+    line = input()
+    if line == "Over":
+        break
+    else:
+        new_line = line.split(' : ')
+        if new_line[0].isdigit():
+            name = new_line[1]
+            phone = new_line[0]
+        else:
+            name = new_line[0]
+            phone = new_line[1]
+
+        phone_book[name] = phone
+
+for key, value in sorted(phone_book.items()):
+    print(f'{key} -> {value}')
